e245c02c5b416be4619977f837fc9177f25f80fb
Manami Okada <mokada@cpp.edu>
2017-02-15 04:06:59 -0800

    Add mdl and update user displayName

---
+++ js/game.js
@@ -0,0 +1,14 @@
+var Game = (function() {
+
+	var fref;
+	var STATE = {OPEN: 1, JOINED: 2, PROMPTED: 3, COMPLETE: 4};
+	var NOTES = {C, D, E, F, G, A, B}
+
+	return {
+
+		init: function() {
+			
+		}
+
+	}
+})();
--- js/session.js
+++ js/session.js
@@ -1,28 +1,40 @@
 var Session = (function() {
 	var loggedIn = false;
+	var nickName;
 
 	function authStateChangeListener(user) {
 		console.log("Auth state change: ", user);
+		console.log("Entered Name: ", nickName);
+
+		if (user != null) {
+			user.updateProfile({
+				displayName: nickName,
+				photoURL: ''
+			});
+			console.log("User info: ", user.displayName);
+		}
 	}
 
 	/*
 	* Sign in anonymously
 	*/
 	function signInAnonymously() {
+		nickName = document.getElementById('displayName').value;
 		firebase.auth().signInAnonymously();
+	}
+
+	function signOut() {
+		displayName = null;
+		firebase.auth().signOut();
 	}
 
 
 	return {
 		init: function() {
 			firebase.auth().onAuthStateChanged(authStateChangeListener);
-
-			document.getElementById('btnLogin').addEventListener('click', signInAnonymously);
-			document.getElementById('btnLogout').addEventListener('click', function() {
-				firebase.auth().signOut().then(function() {
-					console.log('Signed Out');
-				});
-			});
+			document.getElementById('btnCreate').addEventListener('click', signInAnonymously);
+			document.getElementById('btnJoin').addEventListener('click', signInAnonymously);
+			document.getElementById('btnEnd').addEventListener('click', signOut);
 		}
 	}
 
